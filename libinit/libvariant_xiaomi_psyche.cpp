dbafc6a04ef1d71b681e1c23a09795b6d763c7fd
Flicker372 <flicker372@outlook.com>
2024-08-08 15:54:06 +0800

    psyche: libinit: Add support of .cert hides
    Signed-off-by: Flicker372 <flicker372@outlook.com>
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- libinit/libvariant_xiaomi_psyche.cpp
+++ libinit/libvariant_xiaomi_psyche.cpp
@@ -16,6 +16,7 @@ static const variant_info psyche_global_info = {
     .marketname = "Xiaomi 12X",
     .model = "2112123AG",
     .device = "psyche",
+    .cert = "2112123AG",
     .build_fingerprint = "Xiaomi/psyche_global/psyche:13/TKQ1.221114.001/V816.0.8.0.TLDMIXM:user/release-keys",
     .nfc = true,
 };
@@ -30,6 +31,7 @@ static const variant_info psyche_info = {
     .marketname = "Xiaomi 12X",
     .model = "2112123AC",
     .name = "psyche",
+    .cert = "2112123AC",
     .build_fingerprint = "Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.10.0.TLDCNXM:user/release-keys",
     .nfc = true,
 };
