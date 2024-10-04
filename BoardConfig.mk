446db7b3572884e35f6b2c127b8ed6dd716a154b
Flicker372 <flicker372@outlook.com>
2024-10-04 12:24:49 +0800

    psyche: props: Import udfps location and size props
    - Form Psyche HyperOS
    
    Change-Id: I03b7849d33f296d37a1246fb3404230c24111582
    Signed-off-by: Flicker372 <flicker372@outlook.com>
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,7 @@ TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
 TARGET_OTA_ASSERT_DEVICE := psyche
 
 # Properties
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
