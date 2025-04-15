b92a24931b5ca8e3151cecf0285fe6c4d71f1df3
Flicker372 <flicker372@outlook.com>
2025-04-15 18:11:10 +0800

    psyche: libinit: Set mod_device name to psyche_global for CN SKU
    * MiuiCamera tries to start a custom permissions dialog which
    has dependencies on Mi Security Center,
    * When mod_device name is set to global, it will be skipped
    
    Co-authored-by: lynnrin <lynnrin@lynnrin.moe>
    Change-Id: I1eefaf72d6ed26b96c067b22055e299c2e951c42
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- libinit/libvariant_xiaomi_psyche.cpp
+++ libinit/libvariant_xiaomi_psyche.cpp
@@ -26,7 +26,7 @@ static const variant_info psyche_info = {
     .hwc_value = "",
     .sku_value = "",
 
-    .mod_device = "psyche",
+    .mod_device = "psyche_global",
     .brand = "Xiaomi",
     .device = "psyche",
     .marketname = "Xiaomi 12X",
