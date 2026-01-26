742b7d06bcd6ae8b9dea8e1394e8898ba109eedc
mtkpapa <ebanashka10028@gmail.com>
2026-01-26 15:38:22 +0300

    psyche: Welcome PixelOS
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- custom_psyche.mk
+++ custom_psyche.mk
@@ -8,20 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOs stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from psyche device
 $(call inherit-product, device/xiaomi/psyche/device.mk)
 
-PRODUCT_NAME := lineage_psyche
+PRODUCT_NAME := custom_psyche
 PRODUCT_DEVICE := psyche
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 2112123AG
+PRODUCT_MODEL := Xiaomi 12X
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="psyche_global-user 13 TKQ1.221114.001 V816.0.8.0.TLDMIXM release-keys" \
     BuildFingerprint=Xiaomi/psyche_global/psyche:13/TKQ1.221114.001/V816.0.8.0.TLDMIXM:user/release-keys
+
+# Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
