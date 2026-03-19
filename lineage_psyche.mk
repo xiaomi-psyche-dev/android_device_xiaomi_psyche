38b7f2d520c0364aa6b762497160ba57a83b88d9
mtkpapa <ebanashka10028@gmail.com>
2026-03-19 09:46:50 +0300

    psyche: EvolutionX Bringup
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- lineage_psyche.mk
+++ lineage_psyche.mk
@@ -11,6 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+# Evolution-X Flags
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+WITH_GMS := true
+
 # Inherit from psyche device
 $(call inherit-product, device/xiaomi/psyche/device.mk)
 
